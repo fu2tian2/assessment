47c9bcef663da6a9d6bffeac69a0d508c6b956d4
fu2tian2 <63175406+fu2tian2@users.noreply.github.com>
2020-10-31 17:48:51 +0900

    #1に対応しました。

--- assessment.js
+++ assessment.js
@@ -40,6 +40,7 @@ assessmentButton.onclick = () => {
     tweetDivided.appendChild(anchor);
 }
 
+
 const answers = ['{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
     '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
     '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
